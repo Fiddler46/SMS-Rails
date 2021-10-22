26b58f611e2b99cf9bc35c839bbbd9413a84d12c
Fiddler46 <adhyaksh99@gmail.com>
2021-10-22 17:29:18 +0530

    Used rails generated method and fixed edge case.

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -7,7 +7,7 @@ module Api
         def index
           @teams = Team.all.map do |team|
           response = team.attributes
-          response[:dev_ids] = team.developer_ids
+          response[:developer_ids] = team.developer_ids
           
           response
           end
@@ -18,23 +18,22 @@ module Api
         # GET /api/v1/teams/1
         def show
           response = @team.attributes
-          response[:dev_ids] = @team.developer_ids
+          response[:developer_ids] = @team.developer_ids
           
           render json: response
         end
           
         # POST /api/v1/teams
         def create
-          dev_ids = plus_params[:dev_ids]
-          
-          @team = Team.new(plus_params.except(:dev_ids))
-            
+          # dev_ids = plus_params[:dev_ids]
+          # @team = Team.new(plus_params.except(:dev_ids))
+          @team = Team.new(plus_params)
+          # if @team.save
+          #   unless dev_ids.nil?
+          #   @team.developers << Developer.find(dev_ids)
+          #   @team.save!
+          #   end
           if @team.save
-            unless dev_ids.nil?
-            @team.developers << Developer.find(dev_ids)
-            @team.save!
-            end
-          
             render json: @team, status: :created, location: @team
           else
             render json: @team.errors, status: :unprocessable_entity
@@ -44,16 +43,12 @@ module Api
           
         # PATCH/PUT /api/v1/teams/1
         def update
-          dev_ids = team_params[:dev_ids]
-
-          unless dev_ids.nil?
-            # @team.developers.delete(*@team.developer_ids)
-
-            # @team.developers << Developer.find(dev_ids)
-            @team.developer_ids = dev_ids
-            
-            @team.save!
-          end
+          # dev_ids = team_params[:dev_ids]
+          # byebug
+          # unless dev_ids.nil?
+          #   @team.developer_ids = dev_ids
+          #   @team.save!
+          # end
 
           if @team.update(team_params)
             render json: @team
@@ -75,11 +70,11 @@ module Api
           
         # Only allow a list of trusted parameters through.
         def team_params
-          params.require(:team).permit(:name, :dept_name, dev_ids: [])
+          params.require(:team).permit(:name, :dept_name, developer_ids: [])
         end
           
         def plus_params
-          params.require(:team).permit(:name, :dept_name, dev_ids: [], :developers_attributes => [:full_name, :email, :mobile])
+          params.require(:team).permit(:name, :dept_name, developer_ids: [], :developers_attributes => [:full_name, :email, :mobile])
             
         end
       end
