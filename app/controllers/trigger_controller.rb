b0a0277253000bbf71ea32fa821f3abe6fccf035
Fiddler46 <adhyaksh99@gmail.com>
2021-10-29 12:50:03 +0530

    Replaced some instance varlables for params

--- app/controllers/trigger_controller.rb
+++ app/controllers/trigger_controller.rb
@@ -2,18 +2,15 @@ class TriggerController < ApplicationController
     require 'securerandom'
     def notification
         @message = Message.new(message_params)
-        @id = params[:team_id]
-        @content = params[:content]
-        @title = params[:title]
         @mob = Team.find(params[:team_id]).developers.pluck(:mobile)
         @mail = Team.find(params[:team_id]).developers.pluck(:email)
         tim = Time.now
 
         if @message.save
           respond_to do |format|
-            format.json { render json: { 'Response' => {:team_id => @id, 
-                'sms' => { "id" => SecureRandom.uuid, "mobile" => @mob, :content => @content, "sent_at" => tim }, 
-                'email' => { "id" => SecureRandom.uuid, "email" => @mail, :title => @title, :content => @content, "sent_at" => tim } } } }
+            format.json { render json: { 'Response' => {:team_id => @message.team_id, 
+                'sms' => { "id" => SecureRandom.uuid, "mobile" => @mob, :content => @message.content, "sent_at" => tim }, 
+                'email' => { "id" => SecureRandom.uuid, "email" => @mail, :title => @message.title, :content => @message.content, "sent_at" => tim } } } }
           end
         else
             render json: @message.errors, status: :unprocessable_entity
